0802b5a308dd71086fec277460cd8406abbb5030
em11062007 <129952338+em11062007@users.noreply.github.com>
2024-05-06 08:52:59 -0300

    Update cadastro.py

--- cadastro.py
+++ cadastro.py
@@ -1,1 +1,18 @@
-# trabalho02
+print("faça seu cadastro da pessoa 1 aqui abaixo:")
+sexo1=input("insira seu sexo:")
+idade1=int(input("coloque sua idade:"))
+nome1=input("coloque seu nome:")
+
+print("faça seu cadastro da pessoa 2 aqui abaixo:")
+genero=input("insira seu sexo:")
+valor=int(input("coloque sua idade:"))
+apelido=input("coloque seu nome:")
+
+print("faça seu cadastro da pessoa 3 aqui abaixo:")
+sexo3=input("insira seu sexo:")
+idade3=int(input("coloque sua idade:"))
+nome3=input("coloque seu nome:")
+
+print("pessoa 1:", sexo1,idade1,nome1)
+print("pessoa 2:", genero,valor,apelido)
+print("pessoa 3:", sexo3,idade3,nome3)
