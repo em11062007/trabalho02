95c8c409eade7aa20520749c789727daef6ccf9f
em11062007 <129952338+em11062007@users.noreply.github.com>
2024-05-07 10:48:17 -0300

    Update potencia.py

--- potencia.py
+++ potencia.py
@@ -1,9 +1,6 @@
-base = int(input("digite o valor da base:"))
-expoente = int(input("digite o valor do expoente:"))
-
-r = base
-
-for i in range(1,expoente):
-  r * r = base
-
-print("o valor da potencia é:" ,)
+base =  int(input("digite o valor da base: "))
+expoente = int(input("digite o valor do expoente: "))
+R = base
+for X in range(1,expoente):
+    R = R*base
+print("resultado:", "{:.2f}".format(R))
